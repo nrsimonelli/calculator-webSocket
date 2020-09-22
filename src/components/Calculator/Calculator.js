8fd61c26328124d5341ea7fa5235c4e2312f5e2f
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-22 16:49:59 -0500

    started calculator logic and established base funcitons

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,12 +3,46 @@ import { connect } from 'react-redux';
 import AnswerList from '../AnswerList/AnswerList';
 
 class Calculator extends Component {
+  state = {
+    x: '',
+    operator: '',
+    y: '',
+    onY: false,
+    onOperator: false,
+
+  }
+
+  digitClicked = (event) => {
+    console.log('digit clicked!')
+  }
+  operatorClicked = (event) => {
+    console.log('operator clicked!')
+
+  }
+  equalClicked = () => {
+    console.log('equals clicked!')
+
+  }
+  clearAll = () => {
+
+  }
 
   render() {
     return (
       <div className='root-calculator'>
-        <p>testing calc xxxxx</p>
-        <p>many buttons here</p>
+        <p>Calculate Here</p>
+        <button
+          value={1}
+          onClick={this.digitClicked}
+        >1</button>
+        <button
+          value={'+'}
+          onClick={this.operatorClicked}
+        >+</button>
+        <button
+          value={'='}
+          onClick={this.equalCicked}
+        >=</button>
         <AnswerList />
       </div>
     ) // end return
